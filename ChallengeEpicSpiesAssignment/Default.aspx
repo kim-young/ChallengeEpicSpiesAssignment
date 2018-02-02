c680ba5263caf53f84dfe5d628cf0539c8442d38
Young Kim <younghyun87@gmail.com>
2018-02-02 01:16:45 -0500

    C# Challenge 5

---
+++ ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Spy New Assignment Form</h1>
+            <br />
+            Spy Code Name: <asp:TextBox ID="SpyNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name: <asp:TextBox ID="NewAssignmentTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment<br />
+            <asp:Calendar ID="PreviousEndDateCalendar" runat="server"></asp:Calendar>
+            <br />
+            Start Date of New Assignment<br />
+            <asp:Calendar ID="StartDateCalendar" runat="server"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment<br />
+            <asp:Calendar ID="EndDateCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Generate" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                PreviousEndDateCalendar.SelectedDate = DateTime.Today;
+                StartDateCalendar.SelectedDate = DateTime.Today.AddDays(14);
+                EndDateCalendar.SelectedDate = DateTime.Today.AddDays(21);
+            }
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            //2 weeks of resting time
+            TimeSpan totalRestDays = StartDateCalendar.SelectedDate.Subtract(PreviousEndDateCalendar.SelectedDate);
+
+            if (totalRestDays.TotalDays < 14)
+            {
+                resultLabel.Text = "ERROR: There must be total rest time of 14 days or 2 weeks.";
+                StartDateCalendar.SelectedDate = PreviousEndDateCalendar.SelectedDate.AddDays(14);
+                return;
+            }
+
+
+            // Spies cost $500 per day
+            TimeSpan totalDurationOfAssignment = EndDateCalendar.SelectedDate.Subtract(StartDateCalendar.SelectedDate);
+            double totalCost;
+
+            if (totalDurationOfAssignment.TotalDays > 21)
+            {
+                totalCost = (totalDurationOfAssignment.TotalDays * 500) + 1000;
+            }
+            else
+            {
+                totalCost = totalDurationOfAssignment.TotalDays * 500;
+            }
+
+            resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", 
+                SpyNameTextBox.Text, 
+                NewAssignmentTextBox.Text, 
+                totalCost);
+        }
+    }
+}
